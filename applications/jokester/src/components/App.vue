e11ca82077d29aa6a94a410db8ab17824cafdcad
Ismaïl <nguyen.ismail@gmail.com>
2017-06-04 21:47:04 +0200

    implements mutations and actions within store

---
+++ applications/jokester/src/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <h3>Got jokes ?</h3>
+        <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
+        <div>
+            {{$store.state}}
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import { mapActions } from 'vuex'
+
+    export default {
+        methods: mapActions([
+            'initJokes'
+        ])
+    }
+</script>
+
