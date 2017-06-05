3e86ea7a883d3e48cb32a2b9425f6d780335f046
Ismaïl <nguyen.ismail@gmail.com>
2017-06-05 18:03:52 +0200

    Add Turnout project with firebase authentication

---
+++ applications/turnout/src/firebaseApp.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyC1b2Q4PEymWL5CD9dkNfUtCnr0nzqDc60",
+    authDomain: "turnout-30bd9.firebaseapp.com",
+    databaseURL: "https://turnout-30bd9.firebaseio.com",
+    projectId: "turnout-30bd9",
+    storageBucket: "turnout-30bd9.appspot.com",
+    messagingSenderId: "759765621952"
+};
+
+export const firebaseApp = firebase.initializeApp(config)
